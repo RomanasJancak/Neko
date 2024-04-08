c147a868957d3e5d5794df653f02728aab8c32ac
Romanas Jančak <romanas.jancak@gmail.com>
2024-04-08 13:25:49 +0300

    Dashboard stauts change on move beetween columns version 1

--- resources/views/day/dashboard.blade.php
+++ resources/views/day/dashboard.blade.php
@@ -9,124 +9,102 @@
       </div>
       <div class="row job-dropableListArea">
         @foreach ($jobsUnassigned as $job)
-          <!-- <div class="col-12 border border-dark border-2 rounded" style="background-color: {{$job->status->color_main}};"  id="jobElement-{{$job->id}}"> -->
-          <div class="col-12 border border-dark border-2 rounded draggable" style="background-color: {{$job->status->color_main}};" draggable="true" id="jobElement-{{$job->id}}">
-            <div class="row job-header-div">
-              <div class="col job-id-div">
-                NJ{{$job->id}}
-              </div>
-              <div class="col job-status-div">
-                {{$job->status->name}}
-              </div>   
+        @foreach ($job->tasks as $task)
+        <div class="col-12 border border-dark border-2 rounded draggable" style="background-color: {{$task->job->status->color_main}};" draggable="true" id="taskElement-{{$task->id}}" data-jobid="{{$task->job->id}}">
+          <div class="row job-header-div">
+            <div class="col job-id-div">
+              NJ{{$task->job->id}}
             </div>
-            <div class="row pickup-row border" style="background-color: {{$job->status->color_pickup}};">
-              <div  class="col-2" 
-                    style="
-                      writing-mode: vertical-lr;
-                      transform: rotate(180deg);
-                      text-align: center;
-                      vertical-align: middle;"
-              >PU</div>
-              <div class="col-8">
-                @foreach ($job->tasks as $task)
-                  @isset($task->pickup)                    
-                    <div class="row">
-                    <div class="col">
-                      {{$task->pickup->pickupclientname}}
-                    </div>
-                    </div>
-                    <div class="row"><div class="col">{{ \Illuminate\Support\Carbon::parse($task->pickup->pickup_time_begin)->format('H:i') }}</div><div class="col">{{ \Illuminate\Support\Carbon::parse($task->pickup->pickup_time_end)->format('H:i') }}</div></div>
-                    <div class="row"><div class="col">{{$task->pickup->pickupAddressShort()}}</div></div>
-                  @endisset
-                @endforeach
-              </div>
-              <div  class="col-2" 
-                    style="
-                      writing-mode: vertical-lr;
-                      transform: rotate(180deg);
-                      text-align: center;
-                      vertical-align: middle;"
-              >
-              <button class="button-completeTaskPickup" data-jobid="{{$job->id}}" id="button-completeTaskPickup-{{$job->id}}"><span class="bi bi-check"></span></button>
-              </div>
-            </div>
-            @foreach ($job->tasks as $task)
-              @isset($task->package)
-              <div class="row package-row border" id="package-{{$task->id}}" style="background-color: {{$job->status->color_dropoff}};">
-                <div  class="col-2" 
-                    style="
-                      writing-mode: vertical-lr;
-                      transform: rotate(180deg);
-                      text-align: center;
-                      vertical-align: middle;"
-                  >
-                    DROP
-                </div>
-                <div class="col-10 package" >
-                  <div class="row">
-                    <div class="col-auto">
-                      {{$task->package->dropoff_name}}
-                    </div>
-                  </div>
-                  <div class="row">
-                    <div class="col-6">
-                      {{ \Illuminate\Support\Carbon::parse($task->package->packagedropofftimebegin)->format('H:i') }}
-                    </div>
-                    <div class="col-6">
-                      {{ \Illuminate\Support\Carbon::parse($task->package->packagedropofftimeend)->format('H:i') }}  
-                    </div>
-                  </div>
-                  <div class="row">
-                    <div class="col">
-                      {{$task->package->pickupAddressShort()}}
-                    </div>
-                  </div>
-                </div>
-              </div>
-              @endisset
-            @endforeach
-            @foreach ($job->tasks as $task)
-              @isset($task->return)
-              <div class="row package-row border" id="package-{{$task->id}}" style="background-color: {{$job->status->color_dropoff}};">
-                <div  class="col-2" 
-                    style="
-                      writing-mode: vertical-lr;
-                      transform: rotate(180deg);
-                      text-align: center;
-                      vertical-align: middle;"
-                  >
-                    RETURN
-                </div>
-                <div class="col-10 package" >
-                  <div class="row">
-                    <div class="col-auto">
-                      {{$task->return->name}}
-                    </div>
-                  </div>
-                  <div class="row">
-                    <div class="col-6">
-                      {{ \Illuminate\Support\Carbon::parse($task->return->time_begin)->format('H:i') }}
-                    </div>
-                    <div class="col-6">
-                      {{ \Illuminate\Support\Carbon::parse($task->return->time_end)->format('H:i') }}  
-                    </div>
-                  </div>
-                  <div class="row">
-                    <div class="col">
-                      {{$task->return->addressShort()}}
-                    </div>
-                  </div>
-                </div>
-              </div>
-              @endisset
-            @endforeach
-            <div class="row">
-              <div class="col">
-                <button class="button-split" data-jobid="{{$job->id}}" id="button-split-{{$job->id}}">Split</button>
-                <button class="button-completeJob" data-jobid="{{$job->id}}" id="button-completeJob-{{$job->id}}">Complete</button>
-              </div>
+            <div class="col job-status-div">
+                {{$task->status->name}} [{{$task->order_number}}]
             </div>
           </div>
+          @isset($task->pickup)  
+          <div class="row pickup-row border" style="background-color: {{$task->job->status->color_pickup}};">
+            <div  class="col-2" 
+                    style="
+                      writing-mode: vertical-lr;
+                      transform: rotate(180deg);
+                      text-align: center;
+                      vertical-align: middle;"
+            >
+              PU
+            </div>          
+            <div class="col-8">
+                <div class="row"><div class="col">{{$task->pickup->pickupclientname}}</div></div>
+                <div class="row"><div class="col">{{ \Illuminate\Support\Carbon::parse($task->pickup->pickup_time_begin)->format('H:i') }}</div><div class="col">{{ \Illuminate\Support\Carbon::parse($task->pickup->pickup_time_end)->format('H:i') }}</div></div>
+                <div class="row"><div class="col">{{$task->pickup->pickupAddressShort()}}</div></div>
+            </div>
+            <div class="col-2">
+              <button class="btn btn-success button-completeTask" data-jobid="{{$task->id}}" id="button-completeTask-{{$task->id}}"><span class="bi bi-check"></span></button>
+            </div>
+          </div>
+          @endisset
+          @isset($task->package)
+          <div class="row pickup-row border" style="background-color: {{$task->job->status->color_dropoff}};">
+            <div  class="col-2" 
+                    style="
+                      writing-mode: vertical-lr;
+                      transform: rotate(180deg);
+                      text-align: center;
+                      vertical-align: middle;"
+            >
+              DROP
+            </div>          
+            <div class="col-8">
+                <div class="row"><div class="col">{{$task->package->dropoff_name}}</div></div>
+                <div class="row"><div class="col">{{ \Illuminate\Support\Carbon::parse($task->package->packagedropofftimebegin)->format('H:i') }}</div><div class="col">{{ \Illuminate\Support\Carbon::parse($task->package->packagedropofftimeend)->format('H:i') }}</div></div>
+                <div class="row"><div class="col">{{$task->package->addressShort()}}</div></div>
+            </div>
+            <div class="col-2">
+              <button class="btn btn-success button-completeTask" data-jobid="{{$task->id}}" id="button-completeTask-{{$task->id}}"><span class="bi bi-check"></span></button>
+            </div>
+          </div>
+          @endisset
+          @isset($task->return)
+          <div class="row pickup-row border" style="background-color: {{$task->job->status->color_dropoff}};">
+            <div  class="col-2" 
+                    style="
+                      writing-mode: vertical-lr;
+                      transform: rotate(180deg);
+                      text-align: center;
+                      vertical-align: middle;"
+            >
+              RETURN
+            </div>          
+            <div class="col-8">
+                <div class="row"><div class="col">{{$task->return->name}}</div></div>
+                <div class="row"><div class="col">{{ \Illuminate\Support\Carbon::parse($task->return->time_begin)->format('H:i') }}</div><div class="col">{{ \Illuminate\Support\Carbon::parse($task->return->time_end)->format('H:i') }}</div></div>
+                <div class="row"><div class="col">{{$task->return->addressShort()}}</div></div>
+            </div>
+            <div class="col-2">
+              <button class="btn btn-success button-completeTask" data-jobid="{{$task->id}}" id="button-completeTask-{{$task->id}}"><span class="bi bi-check"></span></button>
+            </div>
+          </div>
+          @endisset
+          @isset($task->customTask)
+          <div class="row pickup-row border" style="background-color: {{$task->job->status->color_dropoff}};">
+            <div  class="col-2" 
+                    style="
+                      writing-mode: vertical-lr;
+                      transform: rotate(180deg);
+                      text-align: center;
+                      vertical-align: middle;"
+            >
+              RETURN
+            </div>          
+            <div class="col-8">
+                <div class="row"><div class="col">{{$task->customTask->name}}</div></div>
+                <div class="row"><div class="col">{{ \Illuminate\Support\Carbon::parse($task->customTask->time_begin)->format('H:i') }}</div><div class="col">{{ \Illuminate\Support\Carbon::parse($task->customTask->time_end)->format('H:i') }}</div></div>
+                <div class="row"><div class="col">{{$task->customTask->addressShort()}}</div></div>
+            </div>
+            <div class="col-2">
+              <button class="btn btn-success button-completeTask" data-jobid="{{$task->id}}" id="button-completeTask-{{$task->id}}"><span class="bi bi-check"></span></button>
+            </div>
+          </div>
+          @endisset
+        </div> 
+        @endforeach
         @endforeach
       </div>
     </div>
@@ -424,13 +402,40 @@ document.addEventListener('DOMContentLoaded', function() {
         targetList.insertBefore(document.getElementById(dropedElementId), targetJob.nextSibling);
       }
       if(event.target.closest('.job-columenToGetDropEvent').id === 'job-column-unassigned'){
-        dropedElement = document.getElementById(dropedElementId);
-        jobId = dropedElementId.replace("jobElement-", ""); // IMPORTANT
-        userId  = event.target.closest('.job-columenToGetDropEvent').id.replace("job-column-usercolumn-", "");
+        userId = 0;
         statusId  = 10;
+        dropedElement = document.getElementById(dropedElementId);
+        // jobId = dropedElementId.replace("jobElement-", ""); // IMPORTANT
+        jobId = dropedElement.getAttribute('data-jobid');
+        taskListAreaElement = event.target.closest('.job-dropableListArea');
+        //console.log(taskListAreaElement);
+        if(taskListAreaElement){
+          tasks = taskListAreaElement.querySelectorAll('.draggable');
+          tasks.forEach(function(task,index){
+            //console.log(task.id.replace("taskElement-", ""));
+            updateTaskCourierAndStatus(task.id.replace("taskElement-", ""),userId,statusId,index+1);
+          });
+        }else{
+          //console.log(dropedElement);
+          updateTaskCourierAndStatus(dropedElement.id.replace("taskElement-", ""),userId,statusId,1);
+          allOtherJobTasksElements = document.querySelectorAll('.draggable[data-jobid="' + jobId + '"]');
+          var copiedNodeList = document.createDocumentFragment();
+          allOtherJobTasksElements.forEach(function(task,index){
+            if(task.id.replace("taskElement-", "") == dropedElement.id.replace("taskElement-", "")){
+            }else{
+              copiedNodeList.appendChild(task.cloneNode(true));
+            }
+          });
+          console.log(copiedNodeList);
+          var copiedNodeArray = Array.from(copiedNodeList.childNodes);
+          copiedNodeArray.forEach(function(task,index){
+            updateTaskCourierAndStatus(task.id.replace("taskElement-", ""),userId,statusId,index+2);
+          });
+        }
+        
         //console.log(userId+' '+jobId);
         updateJobCourierAndStatus(jobId,'none',statusId);
-        const routeUrl = "{{ route('status.getStatusInfo', ['id' => ':id']) }}".replace(':id', 10);
+        const routeUrl = "{{ route('status.getStatusInfo', ['id' => ':id']) }}".replace(':id', statusId);
         fetch(routeUrl)
             .then(response => response.json())
             .then(data => {
@@ -451,6 +456,7 @@ document.addEventListener('DOMContentLoaded', function() {
         });
 
       }else if(/^job-column-usercolumn-\d+$/.test(event.target.closest('.job-columenToGetDropEvent').id)){
+        statusId  = 13;
         dropedElement = document.getElementById(dropedElementId);
         //jobId = dropedElementId.replace("jobElement-", "");
         jobId = dropedElement.getAttribute('data-jobid');
@@ -461,31 +467,30 @@ document.addEventListener('DOMContentLoaded', function() {
           tasks = taskListAreaElement.querySelectorAll('.draggable');
           tasks.forEach(function(task,index){
             //console.log(task.id.replace("taskElement-", ""));
-            updateTaskCourierAndStatus(task.id.replace("taskElement-", ""),userId,0,index+1);
+            updateTaskCourierAndStatus(task.id.replace("taskElement-", ""),userId,statusId,index+1);
           });
         }else{
           //console.log(dropedElement);
-          updateTaskCourierAndStatus(dropedElement.id.replace("taskElement-", ""),userId,0,1);
+          updateTaskCourierAndStatus(dropedElement.id.replace("taskElement-", ""),userId,statusId,1);
           allOtherJobTasksElements = document.querySelectorAll('.draggable[data-jobid="' + jobId + '"]');
           var copiedNodeList = document.createDocumentFragment();
           allOtherJobTasksElements.forEach(function(task,index){
             if(task.id.replace("taskElement-", "") == dropedElement.id.replace("taskElement-", "")){
             }else{
-              //updateTaskCourierAndStatus(task.id.replace("taskElement-", ""),userId,0,index+2);
               copiedNodeList.appendChild(task.cloneNode(true));
             }
           });
           console.log(copiedNodeList);
           var copiedNodeArray = Array.from(copiedNodeList.childNodes);
           copiedNodeArray.forEach(function(task,index){
-            updateTaskCourierAndStatus(task.id.replace("taskElement-", ""),userId,0,index+2);
+            updateTaskCourierAndStatus(task.id.replace("taskElement-", ""),userId,statusId,index+2);
           });
         }
-        statusId  = 13;
+        
         //console.log(userId+' '+jobId);
         updateJobCourierAndStatus(jobId,userId,statusId);
         //updateTaskCourierAndStatus(taskId,userId,statusId,order_number)
-        taskId = dropedElementId.replace("taskElement-", "");
+        //taskId = dropedElementId.replace("taskElement-", "");
         //updateTaskCourierAndStatus(taskId,userId,13,1);
         const routeUrl = "{{ route('status.getStatusInfo', ['id' => ':id']) }}".replace(':id', statusId);
         fetch(routeUrl)
@@ -550,15 +555,19 @@ function updateJobCourierAndStatus(jobId,userId,statusId){
             // Optionally handle errors, e.g., display an error message
         });
 }
-function updateTaskCourierAndStatus(taskId,userId,statusId,order_number){
-          // Prepare the data to send in the request body
+function updateTaskCourierAndStatus(taskId,userId = 0,statusId,order_number){
+  // console.log('Function updateTaskCourierAndStatus parameters :');
+  // console.log('taskId : '+taskId);
+  // console.log('userId : '+userId);
+  // console.log('statusId : '+statusId);
+  // console.log('order_number : '+order_number);
           const data = {
             id: taskId,
             courrier_id: userId,
             status_id: statusId,
             order_number: order_number,
         };
-        console.log(data);
+        // console.log(data);
         const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
         // Send a POST request to the server using the generated route
         fetch('{{ route("task.update") }}', { // Blade syntax to generate the route URL
