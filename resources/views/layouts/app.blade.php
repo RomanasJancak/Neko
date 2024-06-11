93ea22911c12d0857813d807e7ba84a4b7adadac
Romanas Jančak <romanas.jancak@gmail.com>
2024-06-11 10:33:48 +0300

    Issue 14

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -105,7 +105,6 @@
                                     @can('client-create')
                                     <li><a class="dropdown-item" href="{{route('job.create')}}">Create pickup</a></li>
                                     <li><a class="dropdown-item" href="{{route('job.create', ['customjob' => true]) }}">Create custom</a></li>
-                                    <li><a class="dropdown-item" href="{{route('job.assign')}}">Assign</a></li>
                                     @endcan
                                 </ul>
                             </div>
