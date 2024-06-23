96f98e88d6da591b4d633679af833dd06a20d5d9
Romanas Jančak <romanas.jancak@gmail.com>
2024-06-23 12:31:41 +0300

    Distance calculatio : without external library

--- app/Models/Distance.php
+++ app/Models/Distance.php
@@ -5,14 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-
+use Illuminate\Support\Facades\Http;
 
 class Distance extends Model
 {
     use HasFactory;
 
-
-protected $fillable = [
+    protected $apiKey;
+    protected $fillable = [
         'origin_address',
         'destination_address',
         'origin_coordinates',
@@ -20,8 +20,39 @@ protected $fillable = [
         'mode_of_travel',
         'distance',
     ];
-
     public static function getDistance($origin, $destination, $mode = 'walking')
+    {
+        $url = 'https://maps.googleapis.com/maps/api/distancematrix/json';
+        $distance = self::where('origin_address', $origin)
+        ->where('destination_address', $destination)
+        ->where('mode_of_travel', $mode)
+        ->first();
+        if (!$distance) {
+                // If distance doesn't exist in the database, fetch from Google Maps API
+                //$distance = self::fetchAndStoreDistance($origin, $destination, $mode);
+            
+            $response = Http::get($url, [
+                'origins' => $origin,
+                'destinations' => $destination,
+                'mode'  =>  $mode,
+                'key' => env('GOOGLE_MAPS_API_KEY'),
+            ]);
+            $data = $response->json();
+            if (isset($data['rows'][0]['elements'][0]['distance']['value'])) {
+                $distance = $data['rows'][0]['elements'][0]['distance']['value'];
+                Distance::create([
+                    'origin_address' => $origin,
+                    'destination_address' => $destination,
+                    'mode_of_travel' => $mode,
+                    'distance' => $distance,
+                ]);
+            }else{
+                $distance = 0;
+            }
+        }
+        return $distance;
+    }
+    public static function getDistance2($origin, $destination, $mode = 'walking')
     {
         $distance = self::where('origin_address', $origin)
             ->where('destination_address', $destination)
