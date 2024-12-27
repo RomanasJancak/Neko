5ed4b8a6893c0decfabf83a4a360675dcf45c431
Romanas Jančak <romanas.jancak@gmail.com>
2024-12-27 06:46:06 +0200

    feat(PackageType): Add extras relationship to PackageType model

--- app/Models/PackageType.php
+++ app/Models/PackageType.php
@@ -18,7 +18,12 @@ class PackageType extends Model
         return $this->hasMany(Package::class);
     }
     public function addOnRules()
-{
+    {
     return $this->belongsToMany(AddOnRule::class)->withTimestamps();
-}
+    }
+    public function extras()
+    {
+        return $this->hasMany(AddOn::class, 'model_id')
+                    ->where('model_type', '=', self::class);
+    }
 }
