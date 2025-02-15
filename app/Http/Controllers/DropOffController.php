bbc92afca7f686745d417e08ecee2d465448fbfc
Romanas Jančak <romanas.jancak@gmail.com>
2025-02-15 06:52:41 +0200

    feat(dropoff): Add DropOff resource with controller, requests, model, policy, migration, factory, and seeder

---
+++ app/Http/Controllers/DropOffController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DropOff;
+use App\Http\Requests\StoreDropOffRequest;
+use App\Http\Requests\UpdateDropOffRequest;
+
+class DropOffController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreDropOffRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(DropOff $dropOff)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(DropOff $dropOff)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateDropOffRequest $request, DropOff $dropOff)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(DropOff $dropOff)
+    {
+        //
+    }
+}
