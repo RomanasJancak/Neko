122c24a481b807ad61472e08c89f0e6051d6af60
Romanas Jančak <romanas.jancak@gmail.com>
2025-02-16 11:11:03 +0200

    feat(vite): Add job index.js to input and configure terser for build minification

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
+                'resources/js/job/index.js',
                 'resources/js/task/show.js',
                 'resources/js/routes.js',
                 'resources/sass/app.scss',
@@ -16,5 +17,18 @@ export default defineConfig({
     server: {
         // Make sure this is not active in production
         //hmr: false, // Disable HMR (Hot Module Replacement) in production
-      },
+    },
+    build: {
+        minify: 'terser', // Use 'terser' for minification
+        terserOptions: {
+            compress: {
+                dead_code: false,
+                drop_console: true, // Remove console logs
+            },
+            // mangle: {
+            //     // Keep function names
+            //     keep_fnames: true,
+            // },
+        },
+    },
 });
