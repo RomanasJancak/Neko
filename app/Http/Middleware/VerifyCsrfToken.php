c488af9496c8cc82c7a3aa7ad080f458c638bbe2
Romanas Jančak <romanas.jancak@gmail.com>
2025-05-26 19:15:42 +0300

    chore: remove unused imports from web routes file

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,6 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         //
-        'jobs/storeFromString', // not important
+        //'jobs/storeFromString', // not important
     ];
 }
