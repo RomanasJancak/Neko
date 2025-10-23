d048f6da1f09fe41049164115ff120573a37318c
RomanasJancak <romanas.jancak@gmail.com>
2025-10-23 10:39:31 +0300

    feat(migrations): create invoices and invoice_items tables; update jobs table with invoice_item_id foreign key

--- database/migrations/2023_09_01_034750_create_jobs_table.php
+++ database/migrations/2023_09_01_034750_create_jobs_table.php
@@ -37,7 +37,8 @@ return new class extends Migration
             $table->string('distance_calculation_method')->default('optimal');
             $table->foreignId('job_template_id')->nullable()->constrained()->onDelete('set null');
             $table->index('job_template_id');
-            $table->foreign('invoice_item_id')->references('id')->on('invoice_items')->onDelete('set null');
+            $table->foreignId('invoice_item_id')->nullable()->constrained('invoice_items')->nullOnDelete();
+
 
         });
     }
