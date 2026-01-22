24cae14344e0d25e362d41086b717d709811be7b
RomanasJancak <romanas.jancak@gmail.com>
2026-01-22 15:43:58 +0200

    Refactor API routes to group user and worklod routes under auth middleware
    - Added UserController and WorklodController to handle user and worklod related routes.
    - Grouped all routes under the 'auth:sanctum' middleware for better organization and security.
    - Created API resource routes for jobs, users, and worklods.
    - Added specific routes for managing worklods associated with users.

--- routes/api.php
+++ routes/api.php
@@ -4,20 +4,15 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Api\JobController as JobController;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\WorklodController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+  Route::apiResource('jobs', JobController::class);
+  Route::apiResource('users', UserController::class);
+  Route::apiResource('worklods', WorklodController::class);
+  Route::get('users/{user}/worklods', [UserController::class, 'worklods'])->name('users.worklods.index');
+  Route::get('users/{user}/worklods/{date}', [UserController::class, 'getWorklodByDate'])->name('users.worklods.showByDate');
+  Route::post('users/{user}/worklods', [UserController::class, 'assignWorklod'])->name('users.worklods.store');
+  Route::delete('users/{user}/worklods/{worklod}', [UserController::class, 'removeWorklod'])->name('users.worklods.destroy');
 });
-Route::apiResource('jobs', JobController::class);
-  
