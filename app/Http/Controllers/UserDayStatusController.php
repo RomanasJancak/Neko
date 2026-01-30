ea7514d2c744f54cfd1f4175609fa8ba5d323dd6
RomanasJancak <romanas.jancak@gmail.com>
2026-01-30 12:58:39 +0200

    feat(UserStatus): add UserStatus and UserDayStatus models, controllers, requests, policies, factories, migrations, and seeders

---
+++ app/Http/Controllers/UserDayStatusController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreUserDayStatusRequest;
+use App\Http\Requests\UpdateUserDayStatusRequest;
+use App\Models\UserDayStatus;
+
+class UserDayStatusController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUserDayStatusRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(UserDayStatus $userDayStatus)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(UserDayStatus $userDayStatus)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateUserDayStatusRequest $request, UserDayStatus $userDayStatus)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(UserDayStatus $userDayStatus)
+    {
+        //
+    }
+}
