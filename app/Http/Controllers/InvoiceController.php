d7dc36afc5609de0fddc1fa7acc08ebf731fa7a0
RomanasJancak <romanas.jancak@gmail.com>
2026-03-23 12:07:29 +0200

    fix(InvoiceController): update email recipient in sendEmail method

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -195,9 +195,9 @@ class InvoiceController extends Controller
 
       $pdfContent = Pdf::loadView('invoice.pdf', $viewData)->output();
       $pdfFileName = 'invoice_' . ($snapshotData['invoice']['invoice_number'] ?? $invoice->id) . '_v' . $snapshot->version . '.pdf';
-      $clientEmail = $invoice->client->email;
-      //$clientEmail = 'shdget@sharedbudget.lt';
-      $clientEmail = 'nekohomedelivery@gmail.com';
+      //$clientEmail = $invoice->client->email;
+      $clientEmail = 'shdget@sharedbudget.lt';
+      //$clientEmail = 'nekohomedelivery@gmail.com';
       Mail::to($clientEmail)->send(
         new InvoiceSendMail(
           $invoice,
