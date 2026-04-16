445012e9a340d49fa103ea34018197151d4595e0
RomanasJancak <romanas.jancak@gmail.com>
2026-04-16 10:51:18 +0300

    refactor(api): clean up and organize API route definitions

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,30 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\Api\JobController;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\WorkloadController;
+use App\Http\Controllers\Api\UserDayStatusController;
+use App\Http\Controllers\Api\UserStatusController;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('jobs', JobController::class);
+    Route::apiResource('users', UserController::class);
+    
+    // Workload Routes
+    Route::get('workloads/calendar', [WorkloadController::class, 'calendar']);
+    Route::apiResource('workloads', WorkloadController::class);
+    Route::patch('workloads/{workload}/bike', [WorkloadController::class, 'assignBike']);
+
+    // Specific User Workload associations
+    Route::prefix('users/{user}')->group(function () {
+        Route::get('workloads', [UserController::class, 'workloads']);
+        Route::get('workloads/{date}', [UserController::class, 'getWorkloadByDate']);
+        Route::post('workloads', [UserController::class, 'assignWorkload']);
+        Route::delete('workloads/{workload}', [UserController::class, 'removeWorkload']);
+    });
+    Route::apiResource('user-day-statuses', UserDayStatusController::class);
+Route::apiResource('user-statuses', UserStatusController::class)->parameters([
+    'user-statuses' => 'user_status' // Forces the parameter to be {user_status}
+]);
 });
