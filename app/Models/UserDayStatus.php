7e73d3814a0084a7894fca12f292db4e1ec76942
RomanasJancak <romanas.jancak@gmail.com>
2026-04-16 16:57:19 +0300

    feat(model): create UserDayStatus model for managing user day statuses

---
+++ app/Models/UserDayStatus.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserDayStatus extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['user_id','user_status_id', 'day_id'];
+
+    public function userStatus()
+    {
+        return $this->belongsTo(UserStatus::class, 'user_status_id');
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function day()
+    {
+        return $this->belongsTo(Day::class);
+    }
+    //--------Accessors & Mutators--------//
+    public function statusName() : Attribute
+    {
+        return Attribute::make(
+            get: fn () => $this->userStatus->status->name ?? 'Unknown',
+        );
+    }
+
+}
+
