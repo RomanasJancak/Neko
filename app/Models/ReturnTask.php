6537533f76be4e08a37212bfbcd669d39373db68
RomanasJancak <romanas.jancak@gmail.com>
2026-04-16 12:59:49 +0300

    feat(return-task): add ReturnTask model with address and time window management

--- app/Models/ReturnTask.php
+++ app/Models/ReturnTask.php
@@ -5,9 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Returntask extends Model
+class ReturnTask extends Model
 {
     use HasFactory;
+    protected $table = 'return_tasks';
     protected $fillable = [
         'name',
         'country',
