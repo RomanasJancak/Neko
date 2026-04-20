0cf51dd06b879b5c8c5deff9b3f75345e710e2d3
RomanasJancak <romanas.jancak@gmail.com>
2026-04-20 13:02:29 +0300

    feat(email): update authorization and validation rules in Store and Update EmailAddress requests

--- app/Http/Requests/StoreEmailAddressRequest.php
+++ app/Http/Requests/StoreEmailAddressRequest.php
@@ -11,7 +11,7 @@ class StoreEmailAddressRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,10 @@ class StoreEmailAddressRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+          'email' => 'required|email',
+          'type' => 'required|string',
+          'emailable_id' => 'required|integer',
+          'emailable_type' => 'required|string',
         ];
     }
 }
--- app/Http/Requests/UpdateEmailAddressRequest.php
+++ app/Http/Requests/UpdateEmailAddressRequest.php
@@ -11,7 +11,7 @@ class UpdateEmailAddressRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,10 @@ class UpdateEmailAddressRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+          'email' => 'sometimes|email',
+          'type' => 'sometimes|string',
+          'emailable_id' => 'prohibited',
+          'emailable_type' => 'prohibited',
         ];
     }
 }
